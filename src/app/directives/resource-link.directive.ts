f408f74e3cc9def422eea45d573c67af39e09b5e
elespdn <spadinielena@gmail.com>
2020-01-29 18:03:20 +0100

    fix href link

--- src/app/directives/resource-link.directive.ts
+++ src/app/directives/resource-link.directive.ts
@@ -16,22 +16,12 @@ export class ResourceLinkDirective implements AfterViewInit {
     this.replaceLink(this.el);  // use function, that is defined below
  }
 
- replaceLink(el: ElementRef) {
-   
+ replaceLink(el: ElementRef) {   
   el.nativeElement.querySelectorAll('a').forEach((aElt: HTMLElement) => {
     const IRI = aElt.attributes['href'].value;  // record in a variable IRI the value of @href
     const encodedIRI = encodeURIComponent(IRI);
-    this.renderer.setAttribute(aElt, 'routerLink',`/resources/${encodedIRI}`); // create new attribute @routerLink and assign it a value equal to 'resources/' + var IRI
-    this.renderer.removeAttribute(aElt, 'href');  // delete original @href
+    this.renderer.setAttribute(aElt, 'href',`/resources/${encodedIRI}`); // assign @href it a value equal to 'resources/' + var IRI
     this.renderer.removeAttribute(aElt, 'class'); // delete @class
-
-    /*
-   Something like this would work if we need to change attributes on an element that is in the html
-      this.renderer.setAttribute(this.el.nativeElement, 'routerLink', 'IRI');
-      this.renderer.removeAttribute(this.el.nativeElement, 'class');
-   But it's not fine here, because we want to change an attribute that is inside an innerHTML sent from db
-   */
-
   });
 
 }
