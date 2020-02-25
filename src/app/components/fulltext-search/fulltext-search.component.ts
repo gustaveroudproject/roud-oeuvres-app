45c1bdb8ca3bab15a96c7e97a5ecb5c5328d032c
elespdn <spadinielena@gmail.com>
2020-02-25 18:23:10 +0100

    sort person

--- src/app/components/fulltext-search/fulltext-search.component.ts
+++ src/app/components/fulltext-search/fulltext-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Resource } from 'src/app/models/resource.model';
 import { DataService } from 'src/app/services/data.service';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
   selector: 'or-fulltext-search',
@@ -9,6 +10,8 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class FulltextSearchComponent implements OnInit {
   resources: Resource[];
+  person: Person;
+  persons: Person[] = [];
   constructor(private dataService: DataService) {}
 
   ngOnInit() {}
@@ -18,9 +21,38 @@ export class FulltextSearchComponent implements OnInit {
       this.dataService.fullTextSearch(searchText).subscribe(
         (resources: Resource[]) => {
           this.resources = resources;
+
+          //console.log(resources);
+
+          this.resources.forEach( (resource) => {
+            if (resource.resourceClassLabel == "Person") {
+              this.dataService.getPerson(resource.id)
+              .subscribe(
+                (person: Person) => {
+                this.person = person;
+                //console.log(person);
+                this.persons.push(person);
+                
+              });
+            }
+          }
+          
+          )
+          console.log(this.persons);
+
         },
         error => console.error(error)
       );
     }
+
+    
   }
 }
+
+
+///////////////////////
+const res = [];
+          //   for(r in readResources) {
+          //     res.push(this.readRes2Person(r))
+          //   }
+          //   return res;
