af48090ad12f2b9c63e15375d868b068bc990f0e
elespdn <spadinielena@gmail.com>
2020-04-20 12:33:35 +0200

    created place-page component and added to route

---
+++ src/app/components/place-page/place-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'or-place-page',
+  templateUrl: './place-page.component.html',
+  styleUrls: ['./place-page.component.scss']
+})
+export class PlacePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
