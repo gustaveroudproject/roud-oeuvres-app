0debcf278d7da3254b804ad0656937bfee38486d
elespdn <spadinielena@gmail.com>
2020-05-18 15:07:11 +0200

    add pubTextPage to resource router component

--- src/app/components/resource-router/resource-router.component.ts
+++ src/app/components/resource-router/resource-router.component.ts
@@ -29,6 +29,7 @@ export class ResourceRouterComponent implements OnInit {
         routeMapping.set('Manuscript', '/manuscripts/');
         routeMapping.set('EstablishedText', '/texts/');
         routeMapping.set('Work', '/works/');
+        routeMapping.set('Periodical article', '/archive/pub/text/');
 
         // get resource class
         this.route.paramMap.subscribe(
@@ -38,7 +39,7 @@ export class ResourceRouterComponent implements OnInit {
                 .getResource(decodeURIComponent(params.get('iri')))
                 .subscribe(
                   (resource: Resource) => {
-                    // console.log(resource.resourceClassLabel);
+                      // console.log(resource.resourceClassLabel);
                     if (routeMapping.has(resource.resourceClassLabel)) {
                       // if the class is in the dictionary
                       const resRoutePrefix = routeMapping.get(
