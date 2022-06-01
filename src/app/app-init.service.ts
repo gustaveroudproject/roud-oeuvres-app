01577fc8052eea3634162feda0beebcb416e7199
Loïc Jaouen <jaouen@bluewin.ch>
2022-06-01 17:16:35 +0200

    removing the injection

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -1,7 +1,5 @@
-import { Injectable } from '@angular/core';
 import { KnoraApiConnection, KnoraApiConfig } from '@dasch-swiss/dsp-js';
 
-@Injectable()
 export class AppInitService {
 
     static knoraApiConnection: KnoraApiConnection;
@@ -15,7 +13,14 @@ export class AppInitService {
         return new Promise<void>((resolve, reject) => {
 
             // init knora-api configuration
-            AppInitService.knoraApiConfig = window['tempConfigStorage'] as KnoraApiConfig;
+            const knora = window['tempConfigStorage'].knora;
+            AppInitService.knoraApiConfig = new KnoraApiConfig(
+                knora.apiProtocol, 
+                knora.apiHost, 
+                knora.apiPort, 
+                knora.apiPath, 
+                knora.jsonWebToken, 
+                knora.logErrors);
 
             // set knora-api connection configuration
             AppInitService.knoraApiConnection = new KnoraApiConnection(AppInitService.knoraApiConfig);
