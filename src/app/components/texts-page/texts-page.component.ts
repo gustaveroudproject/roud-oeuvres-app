d579dc7ca8caa36bd578a5473af1ce2febe24c3f
ES <spadinielena@gmail.com>
2022-10-04 23:14:18 +0200

    texts page OK

--- src/app/components/texts-page/texts-page.component.ts
+++ src/app/components/texts-page/texts-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 import { Text, TextLight } from '../../models/text.model';
 
@@ -11,21 +10,27 @@ import { Text, TextLight } from '../../models/text.model';
 export class TextsPageComponent implements OnInit {
   textLights: TextLight[] = [];
   selectedText: Text;
-  index = 0;
+  // index = 0;
 
   constructor(private dataService: DataService) {}
 
-  onLoadNextPage() {
-    this.dataService.getTextLights(this.index).subscribe(
+  //onLoadNextPage() {
+    
+  // }
+
+  
+  ngOnInit() {
+    //this.onLoadNextPage();
+    //this.dataService.getTextLights(this.index).subscribe(
+    
+    /*
+    this.dataService.getTextLights().subscribe(
       (texts: Text[]) => {
         this.textLights.push(...texts);
-        this.index = this.index + 1;
+        //this.index = this.index + 1;
       },
       error => console.error(error)
-    );
-  }
+    );*/
 
-  ngOnInit() {
-    this.onLoadNextPage();
   }
 }
