622a598fed4c7e5a9fe82e3eae7f9f2408b381d8
Loïc Jaouen <jaouen@bluewin.ch>
2022-10-20 16:27:27 +0200

    reduce the amounts of call to the API

--- src/app/components/msc-mss-rewritten-part/msc-mss-rewritten-part.component.ts
+++ src/app/components/msc-mss-rewritten-part/msc-mss-rewritten-part.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { concat } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { MsLight, MsPartLight } from 'src/app/models/manuscript.model';
 import { DataService } from 'src/app/services/data.service';
 
@@ -9,7 +11,7 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class MscMssRewrittenPartComponent implements OnInit {
 
-  mssRewrittenParts: any[];
+  mssRewrittenParts: (MsLight|MsPartLight)[];
   msFromParts4: MsLight;
 
   @Input()
@@ -27,26 +29,33 @@ export class MscMssRewrittenPartComponent implements OnInit {
     this.dataService
     .getManuscriptsRewrittenMsPart(this.msPartId)
     .subscribe((manuscriptsRewrittenParts: MsLight[]) => {
-        this.mssRewrittenParts.push(...manuscriptsRewrittenParts);
-      });
+      this.mssRewrittenParts.push(...manuscriptsRewrittenParts);
+    });
 
     //// get manuscript parts from which this ms part is rewritten
+    let msPartsRewrittenParts: MsPartLight[] = [];
     this.dataService
     .getMsPartsRewrittenMsPart(this.msPartId)
-    .subscribe((msPartsRewrittenParts: MsPartLight[]) => {
-      this.mssRewrittenParts.push(...msPartsRewrittenParts);
-
-      // asynchrone
-      //// get publications from publications' IRIs
-      for (var msPart in msPartsRewrittenParts) {
-        this.dataService
-        .getMsOfMsPart(msPartsRewrittenParts[msPart].isPartOfMsValue)
-        .subscribe(
-          (msFromParts4: MsLight) => {
-            this.msFromParts4 = msFromParts4;
-          });
+    .subscribe(
+      // next: 
+      (parts: MsPartLight[]) => msPartsRewrittenParts.push(...parts),
+      // error:
+      e => console.log(e),      
+      // complete:
+      () => {
+        // push msPartsRewrittenParts into mssRewrittenParts
+        this.mssRewrittenParts.push(...msPartsRewrittenParts);
+        // get the `isPartOfMsValue` of the first part having a `isPartOfMsValue`
+        concat(
+          ...(
+            msPartsRewrittenParts
+            .map(part => part.isPartOfMsValue)
+            .map(msValue => this.dataService.getMsOfMsPart(msValue))
+          )
+        )
+        .pipe(take(1))
+        .subscribe((msFromParts4: MsLight) => { this.msFromParts4 = msFromParts4; })
       }
-    });
-  }
-
+    );
+  }    
 }
